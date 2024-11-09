86053739bfb57cb82070f26030e07837beabcb6a
SdCode000 <shamnad2nk@gmail.com>
2024-11-09 16:42:33 +0530

    Chat App

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,17 +1,18 @@
-// export const db = getFirestore(app);
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDTHwFDAdxZgpNwNSdYa4RAWxyrt2FDC8Y",
-//   authDomain: "sd-s-chat.firebaseapp.com",
-//   projectId: "sd-s-chat",
-//   storageBucket: "sd-s-cat.firebasestorage.app",
-//   messagingSenderId: "9668186329",
-//   appId: "1:966818536329:web:b79ee2e905fff0f89f58"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDTHwFDAdxZgpNwNSdYa4RAWxyrt2FDC8Y",
+  authDomain: "sd-s-chat.firebaseapp.com",
+  projectId: "sd-s-chat",
+  storageBucket: "sd-s-chat.firebasestorage.app",
+  messagingSenderId: "966818536329",
+  appId: "1:966818536329:web:b7669ee2e905fff0f89f58"
+};
 
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app);
